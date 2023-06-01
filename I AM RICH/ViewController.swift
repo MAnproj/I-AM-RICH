c418e268f550fc183054725626cc1ba02d12ba07
ManProj <rmanas.ios12@gmail.com>
2023-06-01 19:34:01 +0530

    Initial Commit

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by ABC on 26/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
